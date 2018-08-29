a1b037986f0cdb36629011c1f927d3a2c7584d5e
Armchair Stravinsky <spid3rbox@gmail.com>
2018-08-29 15:50:02 -0500

    More type parsing troubleshooting

--- lightswitch.py
+++ lightswitch.py
@@ -259,13 +259,13 @@ def off():
 
 
 
-sendCommand([0,128], [255,255,255], 1, controller=fadecandyIPs['officeFC'])
+sendCommand([0,128], [255,255,255], 1, controller='officeFC')
 time.sleep(1)
-sendCommand([0,128], [0,0,0], 1, controller=fadecandyIPs['officeFC'])
+sendCommand([0,128], [0,0,0], 1, controller='officeFC')
 time.sleep(1)
-sendCommand([0,128], [255,255,255], 1, controller=fadecandyIPs['officeFC'])
+sendCommand([0,128], [255,255,255], 1, controller='officeFC')
 time.sleep(1)
-sendCommand([0,128], [0,0,0], 2, controller=fadecandyIPs['officeFC'])
+sendCommand([0,128], [0,0,0], 2, controller='officeFC')
 
 button1last = False
 button2last = False
