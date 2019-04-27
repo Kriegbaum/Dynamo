818f6c37de927f2699965a0222e390adf36a17e6
Armchair Stravinsky <spid3rbox@gmail.com>
2019-04-27 14:41:39 -0500

    Added UI frame to gui experiments
    This should allow the gui to update values at its native framerate, while executing the commands in the background at a slower framerate. This is done to avoid overwhelming the lighting control system with an excessive volume of commands

--- guiTest.py
+++ guiTest.py
@@ -3,6 +3,9 @@ from __future__ import absolute_import
 from Tantallion import *
 import sys
 
+import time
+from threading import Timer
+
 import pygame
 import OpenGL.GL as gl
 
@@ -10,11 +13,42 @@ from imgui.integrations.pygame import PygameRenderer
 import imgui
 import time
 
+class UIframe:
+    def __init__(self, tick):
+        self.tick = tick
+        self.timer = Timer(tick, self.unCache)
+        self.lastUpdate = time.clock()
+        self.commandCache = False
+        self.argsCache = []
+
+    def cache(self, command, args):
+        self.commandCache = command
+        self.argsCache = args
+
+    def unCache(self):
+        if self.commandCache == False:
+            self.lastUpdate = time.clock()
+        else:
+            self.commandCache(*self.argsCache)
+            self.commandCache = False
+            self.lastUpdate = time.clock()
+
+    def update(self):
+        if time.clock() - self.lastUpdate < self.tick:
+            if not self.timer.is_alive():
+                self.timer = Timer(self.tick, self.unCache)
+                self.timer.start()
+        else:
+            self.unCache()
+
+
 patch = Patch()
+
+rgb = patch.fixture('Worklight').getColor()
 
 def interface():
     imgui.new_frame()
-
+    dynaFrame = UIFrame(0.2)
     if imgui.begin_main_menu_bar():
         if imgui.begin_menu("File", True):
             clicked_quit, selected_quit = imgui.menu_item("Quit", 'Cmd+Q', False, True)
@@ -23,15 +57,19 @@ def interface():
             imgui.end_menu()
         imgui.end_main_menu_bar()
 
-    imgui.begin("Fixture Control", True)
-    for f in patch.fixtures:
-        fixture = patch.fixtures[f]
-        if imgui.collapsing_header(fixture.name):
-            if imgui.button('%s On' % fixture.name):
-                fixture.on()
-            imgui.same_line()
-            if imgui.button('%s Off' % fixture.name):
-                fixture.off()
+    imgui.begin("Fixture Control: Worklight", True)
+    if imgui.color_edit3('Worklight', rgb[0], rgb[1], rgb[2])[0]:
+        pass
+    if imgui.v_slider_int('red', 20, 100, rgb[0], 0, 255):
+        pass
+    imgui.same_line(spacing=50)
+    if imgui.v_slider_int('green', 20, 100, rgb[1], 0, 255):
+        pass
+    imgui.same_line(spacing=50)
+    if imgui.v_slider_int('blue', 20, 100, rgb[2], 0, 255):
+        pass
+
+    imgui.same_line(spacing=50)
     imgui.end()
 
 def main():
@@ -52,7 +90,7 @@ def main():
 
             impl.process_event(event)
         interface()
-
+        imgui.show_metrics_window()
         # note: cannot use screen.fill((1, 1, 1)) because pygame's screen
         #       does not support fill() on OpenGL sufraces
         gl.glClearColor(.1, .1, .1, 1)
